1b5649a9d6a1fb430dd59a774a851066fb2c63a4
nanden-infinity <nanden-infinity@hotmail.com>
2025-10-19 02:46:10 +0100

    js was change

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const buttons = document.querySelectorAll("h1 button");
 const description = document.querySelectorAll("dd");
 
-description[2].classList.add("acordion--active");
-buttons[2].classList.add("active");
+description[0].classList.add("acordion--active");
+buttons[0].classList.add("active");
 
 const openAcord = (event, i) => {
   document.querySelector(".active")?.classList.remove("active");
@@ -17,3 +17,53 @@ const openAcord = (event, i) => {
 buttons.forEach((btn, i) =>
   btn.addEventListener("click", (event) => openAcord(event, i))
 );
+
+// const openAcordion = function ( target,index ) {
+//   const {target:button} = target
+// button.onfocus = (target) => {
+//    if(!target){
+//         console.log(document.activeElement === target);
+//         description[index].classList.toggle("acordion--active");
+//    }
+
+//   else{
+//     description[index].classList.remove("acordion--active");
+
+//   }
+// };
+//   const {description} = target
+//   button.classList.toggle('active')
+//   description[index].classList.toggle("acordion--active");
+// };
+
+// buttons.forEach((button,index) => button.addEventListener('click',({target})=>openAcordion({target,description},index,button)))
+
+
+
+// description.forEach(descr => descr.classList.remove("acordion--active"));
+// const openAccordion = (button, index) => {
+//   // alterna a classe ativa no botão
+//   button.classList.toggle("active");
+
+//   // alterna a descrição correspondente
+//   description[index].classList.toggle("acordion--active");
+// };
+
+// // Aplica o evento de clique em todos os botões
+// buttons.forEach((button, index) => {
+//   button.addEventListener("click", () => openAccordion(button, index));
+
+//   // Quando o botão ganha foco (por teclado, tab, etc.)
+//   button.addEventListener("focus", () => {
+//     document.querySelector(".acordion--active")?.classList.remove("acordion--active")
+//     description[index].classList.add("acordion--active");
+//   });
+
+//   // Quando o botão perde foco
+//   button.addEventListener("blur", () => {
+//     document
+//       .querySelector(".acordion--active")
+//       ?.classList.remove("acordion--active");
+//     description[index].classList.remove("acordion--active");
+//   });
+// });
